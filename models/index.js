270ffa99aa91f21507cee7948af9a018e749b11d
okaydokay97 <rubens.fernandes97@gmail.com>
2022-05-18 12:04:48 -0500

    created database tables

--- models/index.js
+++ models/index.js
@@ -1,9 +1,22 @@
 const User = require('./User')
 const Restaurant = require('./Restaurant')
 const Order = require('./Order')
+const Sequelize = require('sequelize')
+const db = require('../config/db.config')
 
-
-
+const sequelize = new Sequelize(db.database, db.user, db.password, {
+  host: db.host,
+  dialect: db.dialect
+})
 
 User.belongsToMany(Restaurant, {through: Order})
 Restaurant.belongsToMany(User, {through: Order})
+
+const tables = {}
+tables.Sequelize = Sequelize
+tables.sequelize = sequelize
+tables.User = require('./User')
+tables.Restaurant = require('./Restaurant')
+tables.Order = require('./Order')
+
+module.exports = tables
--- server.js
+++ server.js
@@ -1,3 +1,26 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const tables = require('./models')
+
+let corsOptions = {
+  origin:'http://localhost:8081'
+}
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+async function createTables(){
+  await tables.User.sync({force: true})
+  await tables.Restaurant.sync({force: true})
+  await tables.Order.sync({force: true})
+}
+app.get('/', (req, res) => {
+  res.json({'message': 'Welcome to Good eats'})
+})
+
+const PORT = process.env.PORT || 4000
+createTables()
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
+
